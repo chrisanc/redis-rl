75a2cdd6228a99cf430f81612166c8088561aae4
chrisanc <cdavidsanchez054@gmail.com>
2026-03-06 11:42:38 -0600

    beta version

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import time
 import random
 import redis
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+data = dict()
+lock = threading.Lock()
 
 class Publisher(threading.Thread):
     def __init__(self):
@@ -11,11 +16,12 @@ class Publisher(threading.Thread):
         self.redis = redis.Redis(host='192.168.68.110', port=6379, db=0)
 
     def run(self):
+        global data
+        data = {"temperature": 20}
         while True:
-            temp = random.uniform(10.0, 40.0)
-            json_datos = json.dumps({"T_target": temp})
+            json_datos = json.dumps({"T_target": data["temperature"]})
             print(f"El publisher acaba de enviar: {json_datos}")
-            self.redis.publish('canal1-2', json_datos)
+            self.redis.publish('canal1_1', json_datos)
             time.sleep(30)
 
 
@@ -25,14 +31,62 @@ class Subscriber(threading.Thread):
         # redis://192.168.68.110:6379
         self.redis = redis.Redis(host='192.168.68.110', port=6379, db=0, decode_responses=True)
         self.pubsub = self.redis.pubsub()
-        self.pubsub.subscribe("canal1-1")
+        self.pubsub.subscribe("canal1_2")
 
     
     def run(self):
+        global data
         while True:
             message = self.pubsub.get_message()
-            print(f"El suscriptor recibio {message}")
+            if message == None or type(message["data"]) == int:
+                continue
+
+            print(f"El suscriptor recibio un mensaje")
+            # Parse the received object to JSON (map)
+            data = json.loads(message["data"])
+            print(data)
+
+            # Sleep the thread for 2 seconds
             time.sleep(1)
+
+
+class Charts(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.x = []
+        self.y = []
+        self.fig, self.ax = plt.subplots()
+        self.line, = self.ax.plot(self.x, self.y, lw=2, marker="o")
+        self.ax.set_xlabel("Tiempo")
+        self.ax.set_ylabel("Temperatura")
+        self.ax.grid(visible=True)
+
+    def init_chart(self):
+        self.line.set_data([], [])
+        return self.line,
+
+    def update(self, frame):
+        with lock:
+            temp = data  # tu variable global
+
+        self.x.append(len(self.x))  # o timestamp
+        self.y.append(temp["temperature"])
+
+        # Ventana deslizante
+        self.ax.set_xlim(max(0, len(self.x) - 100), len(self.x))
+        self.ax.set_ylim(min(self.y) - 2, max(self.y) + 2)
+
+        self.line.set_data(self.x, self.y)
+        return self.line,
+
+    def run(self):
+        self.ani = animation.FuncAnimation(
+            self.fig,
+            self.update,
+            init_func=self.init_chart,
+            interval=1000,  # cada 1 segundo
+            blit=False       # False si cambias los límites del eje
+        )
     
 
 if __name__ == "__main__":
@@ -40,9 +94,15 @@ if __name__ == "__main__":
     pub = Publisher()
     # Instanciate the Subscriber
     sub = Subscriber()
+    chart = Charts()
 
+    # Start the thread execution
     pub.start()
     sub.start()
+    chart.start()
 
+    # Wait 'till they're done
+    plt.show()
     pub.join()
     sub.join()
+    chart.join()
